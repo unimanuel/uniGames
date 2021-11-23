43c880f5883278259190d20a26b176e63719691d
Manuel <unimanuel@yandex.com>
2021-11-23 21:56:06 +0100

    makemove switches colors on click. clearboard. newgame only fills board.

--- src/app/othello-board/othello-board.component.ts
+++ src/app/othello-board/othello-board.component.ts
@@ -8,16 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class OthelloBoardComponent implements OnInit {
 
   squares : any[];
-
+  descr = 'test1';
+  whosturn = 'X';
   constructor() { }
 
   ngOnInit(): void {
     this.squares = Array(64).fill(null);
   }
 
+  newGame() {
+    this.squares = Array(64).fill('O');
+  }
+
+  clearBoard() {
+    this.squares = Array(64).fill(null);
+  }
+
   makeMove(idx: number) {
-    this.squares.splice(idx, 1, 'X');
-    // this.squares[2] = 'X';
+    this.squares.splice(idx, 1, this.whosturn);
+    this.whosturn = this.whosturn == 'X' ? 'O' : 'X';
   }
 
 }
